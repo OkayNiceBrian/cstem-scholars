82a996e8f4631d974c68e1e2e59ff40b9b7cedba
Brian Jackson <brianjackson1224@gmail.com>
2021-10-12 14:41:40 -0700

    Changed span color to green

--- src/templates/switch_roles.php
+++ src/templates/switch_roles.php
@@ -5,8 +5,8 @@ if ($currentRole == "admin") {
     if ($user->isAdvisor()) {
     ?>
         <div>
-            <a href="?redirect=advisors/applications.php">
-                <span style="display: block; padding: 5px; margin-bottom: 5px; border-radius: 5px 5px 5px; border: 1px solid red; background-color: #ffcaca;">
+            <a style="text-decoration: none;" href="?redirect=advisors/applications.php">
+                <span style="display: block; padding: 5px; margin-bottom: 5px; border-radius: 5px 5px 5px; border: 1px solid green; background-color: #98fb98;">
                     You are currently an ADMIN, but you are also an ADVISOR. You can click here to switch to ADVISOR view.
                 </span>
             </a>
@@ -16,8 +16,8 @@ if ($currentRole == "admin") {
     if ($user->isReviewer()) {
     ?>
         <div>
-            <a href="?redirect=reviewers/applications.php">
-                <span style="display: block; padding: 5px; margin-bottom: 5px; border-radius: 5px 5px 5px; border: 1px solid red; background-color: #ffcaca;">
+            <a style="text-decoration: none;" href="?redirect=reviewers/applications.php">
+                <span style="display: block; padding: 5px; margin-bottom: 5px; border-radius: 5px 5px 5px; border: 1px solid green; background-color: #98fb98;">
                     You are currently an ADMIN, but you are also a REVIEWER. You can click here to switch to REVIEWER view.
                 </span>
             </a>
@@ -28,8 +28,8 @@ if ($currentRole == "admin") {
     if ($user->isAdvisor()) {
     ?>
         <div>
-            <a href="?redirect=advisors/applications.php">
-                <span style="display: block; padding: 5px; margin-bottom: 5px; border-radius: 5px 5px 5px; border: 1px solid red; background-color: #ffcaca;">
+            <a style="text-decoration: none;" href="?redirect=advisors/applications.php">
+                <span style="display: block; padding: 5px; margin-bottom: 5px; border-radius: 5px 5px 5px; border: 1px solid green; background-color: #98fb98;">
                     You are currently a REVIEWER, but you are also an ADVISOR. You can click here to switch to ADVISOR view.
                 </span>
             </a>
@@ -39,8 +39,8 @@ if ($currentRole == "admin") {
     if ($user->isAdmin()) {
     ?>
         <div>
-            <a href="?redirect=admin/">
-                <span style="display: block; padding: 5px; margin-bottom: 5px; border-radius: 5px 5px 5px; border: 1px solid red; background-color: #ffcaca;">
+            <a style="text-decoration: none;" href="?redirect=admin/">
+                <span style="display: block; padding: 5px; margin-bottom: 5px; border-radius: 5px 5px 5px; border: 1px solid green; background-color: #98fb98;">
                     You are currently a REVIEWER, but you are also an ADMIN. You can click here to switch to ADMIN view.
                 </span>
             </a>
@@ -51,8 +51,8 @@ if ($currentRole == "admin") {
     if ($user->isReviewer()) {
     ?>
         <div>
-            <a href="?redirect=reviewers/applications.php">
-                <span style="display: block; padding: 5px; margin-bottom: 5px; border-radius: 5px 5px 5px; border: 1px solid red; background-color: #ffcaca;">
+            <a style="text-decoration: none;" href="?redirect=reviewers/applications.php">
+                <span style="display: block; padding: 5px; margin-bottom: 5px; border-radius: 5px 5px 5px; border: 1px solid green; background-color: #98fb98;">
                     You are currently an ADVISOR, but you are also a REVIEWER. You can click here to switch to REVIEWER view.
                 </span>
             </a>
@@ -62,8 +62,8 @@ if ($currentRole == "admin") {
     if ($user->isAdmin()) {
     ?>
         <div>
-            <a href="?redirect=admin/">
-                <span style="display: block; padding: 5px; margin-bottom: 5px; border-radius: 5px 5px 5px; border: 1px solid red; background-color: #ffcaca;">
+            <a style="text-decoration: none;" href="?redirect=admin/">
+                <span style="display: block; padding: 5px; margin-bottom: 5px; border-radius: 5px 5px 5px; border: 1px solid green; background-color: #98fb98;">
                     You are currently an ADVISOR, but you are also an ADMIN. You can click here to switch to ADMIN view.
                 </span>
             </a>
