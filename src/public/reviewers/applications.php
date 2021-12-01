35ef24337ea59cd5940200e8b7c4d0396ab83b9e
Brian Jackson <brianjackson1224@gmail.com>
2021-11-30 17:37:26 -0800

    Updated Email and templates

--- src/public/reviewers/applications.php
+++ src/public/reviewers/applications.php
@@ -28,6 +28,18 @@ if ($c->action() == 'update') {
         #update the applications status, save, then redirect back to the list of applications
         $application->status = 'reviewed';
         $application->save(false);
+        Mail::send(
+           ADMIN_EMAIL,
+            'CSTEM Scholars Grant Application In need of Admin Approval',
+            HTML::template(
+                'emails/application_reviewer_accepted.php',
+                [
+                    'application' => $application,
+                    'period' => $period,
+                    'review' => $review,
+                ]
+            )
+        );
         HTTP::redirect('../reviewers/applications.php');
     }
 }
